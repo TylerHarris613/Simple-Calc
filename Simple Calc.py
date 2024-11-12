fef1fc32e7a5cd1142e2622a7615307be4b3b980
TylerHarris613 <tcharris613@gmail.com>
2024-11-12 12:00:54 -0500

    Testing author username changes

--- Simple Calc.py
+++ Simple Calc.py
@@ -2,3 +2,4 @@
 # Handle Operations: Based on the user's input, perform the corresponding operation.
 # Error Handling: Ensure that division by zero is handled gracefully (give an error message instead of crashing).
 # Display the Result: After the calculation, display the result to the user.
+
